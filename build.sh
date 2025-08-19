39c8d0e0fe5b50f9732f7e603560ae4935072a58
vanshikaverma080 <vvanshika135@gmail.com>
2025-08-19 19:18:14 +0530

    initial commit

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
+#!/usr/bin/env bash
 set -o errexit
 
-
 pip install -r requirements.txt
-
-
-python manage.py collectstatic --no-input
-
-
-python manage.py runserver
-
-
+python manage.py collectstatic --noinput   # <- gathers static files
+python manage.py migrate                   # <- applies DB migrations
